a6e9d622d61d34ef1db918155b4b0c146a69c3f1
abdulkader3 <abdulkader.wabdeveloper@gmail.com>
2024-05-07 00:12:25 +0600

    first

---
+++ Js/script.js
@@ -0,0 +1,49 @@
+let pas = prompt("First thing, first give us your password")
+if(pas == 696969){
+    let fd = prompt('favorite days')
+    if(fd >=100){
+        alert('Congratulations')
+    }
+    else{alert("just get already")}
+}
+else(alert('get lost'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
